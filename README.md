ffddabc64caf400a504b8cc8f8b095d1b7812f8e
karynaur <adityasrinivas.menon@gmail.com>
2021-04-04 01:39:23 +0530

    add files

--- README.md
+++ README.md
@@ -1,2 +1,80 @@
-# jina-gif-search
-Search for gifs 
+
+# Lyrics Live Search: Understanding the Concept of Chunk
+
+[![Jina](https://github.com/jina-ai/jina/blob/master/.github/badges/jina-badge.svg?raw=true  "We fully commit to open-source")](https://get.jina.ai)
+
+[![](demo.gif)](https://www.youtube.com/watch?v=GzufeV8AY_w)
+
+<!-- START doctoc generated TOC please keep comment here to allow auto update -->
+<!-- DON'T EDIT THIS SECTION, INSTEAD RE-RUN doctoc TO UPDATE -->
+**Table of Contents**
+
+- [Use toy data](#use-toy-data)
+- [[Optional] Download full lyrics dataset](#optional-download-full-lyrics-dataset)
+- [Install](#install)
+- [Run](#run)
+- [View in Browser](#view-in-browser)
+- [License](#license)
+
+<!-- END doctoc generated TOC please keep comment here to allow auto update -->
+
+
+## Use toy data
+
+We have included 1000 lyrics as toy data in [`toy-data`](toy-data).
+This data is ready to use with this example.
+
+## [Optional] Download full lyrics dataset
+
+**NOTE**: This is what is used in the Docker image and is **required** if you want to build it (the Docker image) yourself. 
+
+If you want to use the full dataset, you can download it from kaggle (https://www.kaggle.com/neisse/scrapped-lyrics-from-6-genres).
+To get it, once you have your Kaggle Token in your system as described in (https://www.kaggle.com/docs/api), run:
+
+```bash
+bash get_data.sh
+```
+
+## Install
+
+```bash
+pip install -r requirements.txt
+```
+
+## Run
+
+| Command | Description |
+| :--- | :--- |
+| ``python app.py index`` | To index files/data |
+| ``python app.py search`` | To run query on the index |
+| ``python app.py dryrun`` | Sanity check on the topology |
+
+## View in Browser
+
+```bash
+cd static
+python -m http.server
+```
+
+Open `http://0.0.0.0:8000/` in your browser.
+
+## Use Docker image from the jina hub
+
+To make it easier for the user, we have built and published the [Docker image](https://hub.docker.com/r/jinahub/app.example.multireslyricssearch) with 10000 indexed songs (more than the toy example, but just a small part of the huge dataset).
+You can retrieve the docker image using:
+
+```bash
+docker pull jinahub/app.example.multireslyricssearch:0.0.2-0.9.20
+```
+So you can pull from its latest tags.
+
+Then you can run it, and you can proceed to see the results in the browser as explained before
+
+```bash
+docker run -p 65481:65481 jinahub/app.example.multireslyricssearch:0.0.2-0.9.20
+```
+
+
+## License
+
+Copyright (c) 2020-2021 Han Xiao. All rights reserved.
---
+++ app.py
@@ -0,0 +1,91 @@
+__copyright__ = "Copyright (c) 2020 Jina AI Limited. All rights reserved."
+__license__ = "Apache-2.0"
+
+__version__ = '0.0.1'
+
+import csv
+import os
+import sys
+import itertools
+
+from jina.flow import Flow
+from jina import Document
+
+
+def config():
+    parallel = 2 if sys.argv[1] == 'index' else 1
+
+    os.environ.setdefault('JINA_MAX_DOCS', '100')
+    os.environ.setdefault('JINA_PARALLEL', str(parallel))
+    os.environ.setdefault('JINA_SHARDS', str(4))
+    os.environ.setdefault('JINA_WORKSPACE', './workspace')
+    os.environ.setdefault('JINA_PORT', str(65481))
+
+
+def input_fn():
+    lyrics_file = os.environ.setdefault(
+        'JINA_DATA_PATH', 'toy-data/lyrics-toy-data1000.csv'
+    )
+    with open(lyrics_file, newline='', encoding='utf-8') as f:
+        reader = csv.reader(f)
+        for row in itertools.islice(reader, int(os.environ['JINA_MAX_DOCS'])):
+            if row[-1] == 'ENGLISH':
+                with Document() as d:
+                    d.tags['ALink'] = row[0]
+                    d.tags['SName'] = row[1]
+                    d.tags['SLink'] = row[2]
+                    d.text = row[3]
+                yield d
+
+
+# for index
+def index():
+    f = Flow.load_config('flows/index.yml')
+    with f:
+        f.index(input_fn, request_size=8)
+
+
+# for search
+def search():
+    f = Flow.load_config('flows/query.yml')
+
+    with f:
+        f.block()
+
+
+# for test before put into docker
+def dryrun():
+    f = Flow.load_config('flows/query.yml')
+
+    with f:
+        pass
+
+
+def main():
+    if len(sys.argv) < 2:
+        print('choose between "index/search/dryrun" mode')
+        exit(1)
+
+    config()
+    if sys.argv[1] == 'index':
+        workspace = os.environ['JINA_WORKSPACE']
+        if os.path.exists(workspace):
+            print(
+                f'\n +---------------------------------------------------------------------------------+ \
+                    \n |                                   🤖🤖🤖                                        | \
+                    \n | The directory {workspace} already exists. Please remove it before indexing again. | \
+                    \n |                                   🤖🤖🤖                                        | \
+                    \n +---------------------------------------------------------------------------------+'
+            )
+            sys.exit(1)
+        index()
+    elif sys.argv[1] == 'search':
+        search()
+    elif sys.argv[1] == 'dryrun':
+        dryrun()
+    else:
+        raise NotImplementedError(f'unsupported mode {sys.argv[1]}')
+
+
+if __name__ == '__main__':
+    main()
